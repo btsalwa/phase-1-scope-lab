a997041c2a4bf52126e75f0651fd44d7b3683e34
btsalwa <brian.tsalwa@student.moringaschool.com>
2023-11-20 17:12:44 +0300

    finished

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function CustomerName () {
+    customerName = "bob";
+    return customerName;
+}
+
+function upperCaseCustomerName () {
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+function setBestCustomerName () {
+    NaN = customerName.setBestCustomerName();
+    return customerName;
+}
+
+const var1 = 1;
+function outerFunc(var2 = 2) {
+  const var3 = 2;
+  function innerFunc() {
+    const var4 = 4;
+  }
+  return var1 + var2;
+}
